a36e0b37c41334046af26b9e6c9a27f0781c01f2
Wakorithegreat <jkwakori@gmail.com>
2022-05-17 17:52:04 +0100

    lesson2.py integers

---
+++ lesson2.py
@@ -0,0 +1,22 @@
+
+#my age 
+
+age = 18
+no_of_sibling = 4
+
+date_of_birth = 17
+year_of_birth = 2003
+
+my_first_name = "john"
+my_second_name = "wakori"
+
+favorite_food = "prime"
+school_attended = "MFA"
+
+print(" i am " + my_first_name)
+print(" i am " + str(no_of_sibling))
+print(" i am " + str(age) + "years old")
+print(" i am"+ str(date_of_birth))
+print(" i am" + str(year_of_birth))
+print(" i am"+ favorite_food)
+print(" i am" + school_attended)
