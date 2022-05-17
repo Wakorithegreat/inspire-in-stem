70857d39fe683ffaedf3896e4ee1a49511d79442
Wakorithegreat <jkwakori@gmail.com>
2022-05-17 17:22:12 +0100

    lessons

--- lesson1.py
+++ lesson1.py
@@ -1,11 +1,1 @@
-('Hello world')
-
-
-#this is for a
-#multi / many lines
-# comments
-
-print ("my name is john wakori")
-print("i am from nairobi")
-print("i am a kenyan")
-print("i like gaming")
+("hello world")
--- lesson3.py
+++
@@ -1,6 +0,0 @@
-
-f_name = input("what is your name ? ")
-print("my name is " + f_name)
-
-age = input(" how old are you ?")
-print("you are" str(age) + "years old")
