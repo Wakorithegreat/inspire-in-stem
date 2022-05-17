44464816f3d899a8a7752a584078807a920eb135
Wakorithegreat <jkwakori@gmail.com>
2022-05-17 18:02:31 +0100

    lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+("hello world")
+
+#python basics
+
+
+
+print("my name is john wakori")
+print("i am from nairobi")
+print("i am akenyan citizen")
+print("my favorite drink is prime")
+print("i love gaming with the boys")
