a941ed933150faad8aea11d37ff232fb3e6c491f
Wakorithegreat <jkwakori@gmail.com>
2022-05-19 09:14:40 +0100

    loops.py

---
+++ lesson6.py
@@ -0,0 +1,28 @@
+#learning about lists
+motorcycle_owner = "mojo jojo"
+plate_number = ['HL234','YL234','SL234']
+motorcycle = ["honda","yamaya","suzuki"]
+#accessing list items using index
+#print(motorcycle)
+#motorcycle[2] = "buggati" #changing element in a list
+#print( "(i love "  + str((motorcycle[0])))
+#adding an element in a list
+#motorcycle.append ('buggati')#adding item into a list
+#task --- print the  motorcycle and thier plate number
+#print(motorcycle[0],plate_number[0])
+#print(motorcycle[1],plate_number[1])
+#print(motorcycle[2],plate_number[2]
+#deleting item from a list --del
+# del motorcycle[0] #deleting an item from alist
+popped_motorcycle = motorcycle.pop()
+#print(popped_motorcycle)
+#print a statement :my name is mojo jojo and i own a motorcycle plate number
+#print("my name is " + str(motorcycle_owner) + " and i own a " + (motorcycle[0]))
+#print("my name is {} and i own a {} with plate number {} " . format(motorcycle_owner,motorcycle[0], plate_number[0]))
+
+#removing n iten from a list
+motorcycle.remove('yamaha')
+print(motorcycle)
+
+
+
---
+++ loops.py
@@ -0,0 +1,11 @@
+school = ["joy","hope","mercy","happy"]
+pupil = ["peace","patience","amani","mfa"]
+
+#hard way
+#print(f"i am {pupil[0]} and i school at {school[0]}")
+#print(f"i am {pupil[1]} and i school at {school[1]}")
+#print(f"i am {pupil[2]} and i school at {school[2]}")
+#print(f"i am {pupil[3]} and i school at {school[3]}")
+
+for pupil in pupil:
+    print(f"hello i am {pupil}")
