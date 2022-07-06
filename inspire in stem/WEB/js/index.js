ed5ee10a30ccb7240875264c51ea5dfebfa40940
Wakorithegreat <jkwakori@gmail.com>
2022-07-06 08:53:58 +0100

    index.html

---
+++ inspire in stem/WEB/js/index.js
@@ -0,0 +1,15 @@
+let message ="world domination"
+console.log(message)
+document.write(message)
+//single line comment in javascript
+/*
+multi line comment in javascript
+*/
+let num1= 134
+let num2= 134
+let num = num1+num2
+console.log(num)
+
+function submitButton(){
+    alert("button has been clicked")
+}
